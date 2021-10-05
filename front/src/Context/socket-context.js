07e16225e617e04b3d3239d02b9cd95b5f308719
IsraelaZimru <slzimro@gmail.com>
2021-10-05 19:13:35 +0300

    learning out Context

---
+++ front/src/Context/socket-context.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { io } from 'socket.io-client'
+
+
+// const socketContext = React.createContext({
+//     socket: io.connect('http://localhost:5000') // I need to put it in context/global veriable
+// });
+
+// export default socketContext;
